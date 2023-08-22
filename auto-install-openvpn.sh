707e2bab31160dc0f27babf80936d54596032830
112ht <hkh112ht@hotmail.com>
2023-08-22 13:52:19 +0900

    up

--- auto-install-openvpn.sh
+++ auto-install-openvpn.sh
@@ -1,1 +1,33 @@
+#!/bin/bash
 
+INSTALLER_FILE_PATH="./openvpn-install.sh"
+
+
+if [ ! -e $INSTALLER_FILE_PATH ]; then
+  wget https://raw.githubusercontent.com/Angristan/openvpn-install/master/openvpn-install.sh
+  sudo chmod +x $INSTALLER_FILE_PATH
+fi
+
+{
+
+# IP addressの質問に対する応答
+    echo "nlb-open-vpn-01-48da5842352903e1.elb.ap-northeast-1.amazonaws.com"
+# IPv6 supportの質問に対する応答
+    echo "n"
+# Port choiceの質問に対する応答
+    echo "1"
+# Protocolの質問に対する応答
+    echo "2"
+# DNSの質問に対する応答
+    echo "1"
+# Enable compressionの質問に対する応答
+    echo "n"
+# Customize encryption settingsの質問に対する応答
+    echo "n"
+# Press any key to continueの質問に対する応答
+    echo ""
+# Client nameの質問に対する応答
+    echo "Sample-ABC"
+# Client nameの後の質問に対する応答
+    echo "1"
+} | sudo $INSTALLER_FILE_PATH > inslog.log
---
+++ make_user.sh
@@ -0,0 +1,29 @@
+#/bin/bash
+USER_NAME_START_STR="t-u-"
+#ユーザー名開始index
+USER_START_INDEX=1
+#ユーザー名終了index
+USER_END_INDEX=3
+#処理モード 1:ユーザー作成 2:ユーザー削除
+PROCESS_MODE="1"
+
+create_openvpn_user()
+{
+  for i in `seq -f '%04g' $USER_START_INDEX $USER_END_INDEX`
+  do
+      export TEMP_USER_NAME=$USER_NAME_START_STR$i
+      echo "ユーザー作成:"$TEMP_USER_NAME
+      export MENU_OPTION=$PROCESS_MODE
+      export CLIENT=$TEMP_USER_NAME
+      export PASS="1"
+      ./openvpn-install.sh
+#ファイル転送
+#aws s3 cp /home/ec2-user/$CLIENT.ovpn  s3://poc-chikuseki-cloud-s3-for-kk-eds-test-live-01/
+
+done
+}
+
+
+echo "openvpnのクライアント開始"
+create_openvpn_user
+echo "openvpnのクライアント終了"
