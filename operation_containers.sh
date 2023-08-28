ddb89a3dcfcbc1f58f705762beb23b2253e39cc9
112ht <hkh112ht@hotmail.com>
2023-08-28 11:38:43 +0900

    Update operation_containers.sh

--- operation_containers.sh
+++ operation_containers.sh
@@ -102,33 +102,60 @@ con_vpn_docker_container()
 done
 }
 
-# テストファイル作成
-edit_testfile_docker_container()
+# テストスクリプトダウンロード&コンテナーにコピー
+copy_testscript_docker_container()
 {
+  curl -o ~/test_script.sh https://112ht.github.io/openvpn-auto/test_script.sh
   for i in `seq -f '%04g' $USER_START_INDEX $USER_END_INDEX`
   do
       export TEMP_USER_NAME=$USER_NAME_START_STR$i
-      echo "containerランダムテストファイル作成:"$TEMP_USER_NAME
-      # ランダムテストファイル作成
-      docker exec $TEMP_USER_NAME sh -c "base64 /dev/urandom | head -c $RANDOM_TEST_FILE_SIZE > /tmp/${TEMP_USER_NAME}-testfile-${RANDOM_TEST_FILE_SIZE} &"
+      echo "containerにテストスクリプトコピー:"$TEMP_USER_NAME
+      # テストスクリプトコピー
+      docker cp -o ~/test_script.sh $TEMP_USER_NAME:/tmp/test_script.sh
+      docker exec $TEMP_USER_NAME chmod 777 /tmp/test_script.sh
 
 done
 }
 
-# テストファイル送信
-send_testfile_docker_container()
+# テストスクリプト実行する。
+execute_testfile_docker_container()
 {
   for i in `seq -f '%04g' $USER_START_INDEX $USER_END_INDEX`
   do
       export TEMP_USER_NAME=$USER_NAME_START_STR$i
-      echo "containerテストファイル送信:"$TEMP_USER_NAME
-      # ランダムテストファイル送信
-      #TODO
-      #echo "scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=ERROR -i $SSH_KEY_FILE_PATH /tmp/${TEMP_USER_NAME}-testfile-${RANDOM_TEST_FILE_SIZE} ${SSH_USER_ID}@${SSH_SEND_IP}:${SSH_SEND_PATH} "
-      docker exec $TEMP_USER_NAME sh -c "scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=ERROR -i $SSH_KEY_FILE_PATH /tmp/${TEMP_USER_NAME}-testfile-${RANDOM_TEST_FILE_SIZE} ${SSH_USER_ID}@${SSH_SEND_IP}:${SSH_SEND_PATH} &"
+      echo "containerテストスクリプト実行:"$TEMP_USER_NAME
+      #テストスクリプト実行する。
+      docker exec $TEMP_USER_NAME sh -c "/tmp/test_script.sh &"
 
 done
 }
+# # テストファイル作成
+# edit_testfile_docker_container()
+# {
+#   for i in `seq -f '%04g' $USER_START_INDEX $USER_END_INDEX`
+#   do
+#       export TEMP_USER_NAME=$USER_NAME_START_STR$i
+#       echo "containerランダムテストファイル作成:"$TEMP_USER_NAME
+#       # ランダムテストファイル作成
+#       docker exec $TEMP_USER_NAME sh -c "base64 /dev/urandom | head -c $RANDOM_TEST_FILE_SIZE > /tmp/${TEMP_USER_NAME}-testfile-${RANDOM_TEST_FILE_SIZE} &"
+
+# done
+# }
+
+# # テストファイル送信
+# send_testfile_docker_container()
+# {
+#   for i in `seq -f '%04g' $USER_START_INDEX $USER_END_INDEX`
+#   do
+#       export TEMP_USER_NAME=$USER_NAME_START_STR$i
+#       echo "containerテストファイル送信:"$TEMP_USER_NAME
+#       # ランダムテストファイル送信
+#       #TODO
+#       #echo "scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=ERROR -i $SSH_KEY_FILE_PATH /tmp/${TEMP_USER_NAME}-testfile-${RANDOM_TEST_FILE_SIZE} ${SSH_USER_ID}@${SSH_SEND_IP}:${SSH_SEND_PATH} "
+#       docker exec $TEMP_USER_NAME sh -c "scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=ERROR -i $SSH_KEY_FILE_PATH /tmp/${TEMP_USER_NAME}-testfile-${RANDOM_TEST_FILE_SIZE} ${SSH_USER_ID}@${SSH_SEND_IP}:${SSH_SEND_PATH} &"
+
+# done
+# }
 
 echo "openvpn接続用container操作開始"
 
@@ -157,11 +184,11 @@ case "$1" in
     rm_docker_container)
         rm_docker_container
         ;;
-    edit_testfile_docker_container)
-        edit_testfile_docker_container
+    copy_testscript_docker_container)
+        copy_testscript_docker_container
         ;;
-    send_testfile_docker_container)
-        send_testfile_docker_container
+    execute_testfile_docker_container)
+        execute_testfile_docker_container
         ;;
         
     *)
