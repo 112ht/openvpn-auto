07dd183d1a1890584dfb28748c0abbd9bdf5b5da
112ht <hkh112ht@hotmail.com>
2023-08-29 14:56:19 +0900

    Update server03_801-1200.sh

--- vpn_test/server03_801-1200.sh
+++ vpn_test/server03_801-1200.sh
@@ -1,1 +1,60 @@
 
+wait_times=300
+
+./operation_containers-0801-0850.sh
+
+# 5分間待ち
+for i in {1.."${wait_times}"}
+do
+   echo "実行待ち"${i}"秒"
+   sleep 1
+done
+./operation_containers-0851-0900.sh
+
+# 5分間待ち
+for i in {1.."${wait_times}"}
+do
+   echo "実行待ち"${i}"秒"
+   sleep 1
+done
+./operation_containers-0901-0950.sh
+
+# 5分間待ち
+for i in {1.."${wait_times}"}
+do
+   echo "実行待ち"${i}"秒"
+   sleep 1
+done
+./operation_containers-0951-1000.sh
+
+# 5分間待ち
+for i in {1.."${wait_times}"}
+do
+   echo "実行待ち"${i}"秒"
+   sleep 1
+done
+./operation_containers-1001-1050.sh
+
+# 5分間待ち
+for i in {1.."${wait_times}"}
+do
+   echo "実行待ち"${i}"秒"
+   sleep 1
+done
+./operation_containers-1051-1100.sh
+
+# 5分間待ち
+for i in {1.."${wait_times}"}
+do
+   echo "実行待ち"${i}"秒"
+   sleep 1
+done
+./operation_containers-1101-1150.sh
+
+# 5分間待ち
+for i in {1.."${wait_times}"}
+do
+   echo "実行待ち"${i}"秒"
+   sleep 1
+done
+./operation_containers-1151-1200.sh
