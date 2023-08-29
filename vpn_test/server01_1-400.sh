f3e558e2fea2c0ec014f65443498453a7664fbb3
112ht <hkh112ht@hotmail.com>
2023-08-29 14:43:19 +0900

    Create server01_1-400.sh

---
+++ vpn_test/server01_1-400.sh
@@ -0,0 +1,61 @@
+
+
+wait_times=300
+
+./operation_containers-0001-0050.sh
+
+# 5分間待ち
+for i in {1.."${wait_times}"}
+do
+   echo "実行待ち"${i}"秒"
+   sleep 1
+done
+./operation_containers-0051-0100.sh
+
+# 5分間待ち
+for i in {1.."${wait_times}"}
+do
+   echo "実行待ち"${i}"秒"
+   sleep 1
+done
+./operation_containers-0101-0150.sh
+
+# 5分間待ち
+for i in {1.."${wait_times}"}
+do
+   echo "実行待ち"${i}"秒"
+   sleep 1
+done
+./operation_containers-0151-0200.sh
+
+# 5分間待ち
+for i in {1.."${wait_times}"}
+do
+   echo "実行待ち"${i}"秒"
+   sleep 1
+done
+./operation_containers-0201-0250.sh
+
+# 5分間待ち
+for i in {1.."${wait_times}"}
+do
+   echo "実行待ち"${i}"秒"
+   sleep 1
+done
+./operation_containers-0251-0300.sh
+
+# 5分間待ち
+for i in {1.."${wait_times}"}
+do
+   echo "実行待ち"${i}"秒"
+   sleep 1
+done
+./operation_containers-0301-0350.sh
+
+# 5分間待ち
+for i in {1.."${wait_times}"}
+do
+   echo "実行待ち"${i}"秒"
+   sleep 1
+done
+./operation_containers-0351-0400.sh
